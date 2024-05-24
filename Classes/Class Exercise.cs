1b3174e9a45e096ef9106d8b959880c554846205
Kylie Paykoff <kyliepaykoff@gmail.com>
2024-05-24 19:27:00 -0400

    initial commit

---
+++ Classes/Class Exercise.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes
+{
+
+
+
+    public class Car
+    {
+        public Car()
+        { }
+     
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+    }
+
+
+
+
+
+
+
+
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,56 @@
-﻿namespace Classes
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Classes
 {
+
     public class Program
     {
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var myCar = new Car();
+            myCar.Make = "Toyota";
+            myCar.Model = "Corolla";
+            myCar.Year = 2020;
+
+            var carList = new List<Car>() { myCar };
+            foreach (var car in carList)
+            {
+                Console.WriteLine($"{myCar.Make} {car.Model} {myCar.Year}");
+            }
+
+
+
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
+
+
+
+
+
+
+
+
+
 }
